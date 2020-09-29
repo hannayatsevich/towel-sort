fe5a26e916c4cb09a709dd26a0e3b4b5c6c1c3a4
hannayatsevich <hannayatsevich@gmail.com>
2020-09-29 22:40:59 +0300

    feat: implement the 'towel sort' algorithm

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  return matrix ? matrix.reduce((acc, element, index) => {
+    if(index%2 !== 0) element.sort((a, b) => b - a);
+    element.forEach(num => acc.push(num))
+    return acc;
+  }, []) : [];
 }
